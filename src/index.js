f7fc6677882253417062693b3be0284fbc205a52
Димас <52570598+GektoKot@users.noreply.github.com>
2021-06-02 16:14:17 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("ghmghmghm")
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
+function reset() {
+    count = 0
+    countEl.textContent = count
+}
+
+function decrement() {
+    count--
+    countEl.textContent = count
+}
